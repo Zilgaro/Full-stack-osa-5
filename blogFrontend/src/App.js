8d430988f8b69bb5593f4a3d9536c6af0718d1d7
Verner <verner.o.saaristo@accenture.com>
2018-02-21 12:09:24 +0200

    Tehtävä 5.1

--- blogFrontend/src/App.js
+++ blogFrontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+        password: '',
+        username: '',
+        user: null,
+        error: null
     }
   }
 
@@ -14,9 +20,70 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
-  } 
+  }
 
-  render() {
+  handleLoginFieldChange = (event) => {
+      if (event.target.name === 'password') {
+            this.setState({ password: event.target.value })
+      } else if (event.target.name === 'username') {
+            this.setState({ username: event.target.value })
+      }
+  }
+
+    login = async (event) => {
+        event.preventDefault()
+        try{
+            const user = await loginService.login({
+                username: this.state.username,
+                password: this.state.password
+            })
+
+            this.setState({ username: '', password: '', user})
+        } catch(exception) {
+            this.setState({
+                error: 'käyttäjätunnus tai salasana virheellinen',
+            })
+            setTimeout(() => {
+                this.setState({ error: null })
+            }, 5000)
+        }
+    }
+
+
+    render() {
+        const loginForm = () => (
+            <div>
+              <h2>Kirjaudu</h2>
+              <Notification message={this.state.error} />
+              <form onSubmit={this.login}>
+                <div>
+                  käyttäjätunnus
+                  <input
+                      type="text"
+                      name="username"
+                      value={this.state.username}
+                      onChange={this.handleLoginFieldChange}
+                  />
+                </div>
+                <div>
+                  salasana
+                  <input
+                      type="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleLoginFieldChange}
+                  />
+                </div>
+                <button type="submit">kirjaudu</button>
+              </form>
+            </div>
+        )
+
+        if (this.state.user === null) {
+            return (
+                loginForm()
+            )
+        }
     return (
       <div>
         <h2>blogs</h2>
@@ -27,5 +94,4 @@ class App extends React.Component {
     );
   }
 }
-
 export default App;
