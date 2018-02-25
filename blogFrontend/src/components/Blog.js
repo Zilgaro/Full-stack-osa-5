e07e41269be0656af782374df3595ce9515855b9
Verner <verner.o.saaristo@accenture.com>
2018-02-25 16:46:47 +0200

    Korjattu tykkääminen mahdolliseksi jos ei käyttäjää sidottuna blogiin

--- blogFrontend/src/components/Blog.js
+++ blogFrontend/src/components/Blog.js
@@ -6,7 +6,8 @@ class Blog extends React.Component {
         super(props)
         this.state = {
             visible: false,
-            likes: this.props.blog.likes
+            likes: this.props.blog.likes,
+            name: this.props.blog.user === undefined ? '' : this.props.blog.user.name
         }
     }
 
@@ -17,13 +18,24 @@ class Blog extends React.Component {
     likeBlog = async (event) => {
         event.preventDefault()
 
-        const updateBlog = {
-          user: this.props.blog.user._id,
-          title: this.props.blog.title,
-            author: this.props.blog.author,
-            likes: this.state.likes + 1,
-            url: this.props.blog.url
+        let updateBlog
 
+        if (this.props.blog.user === undefined) {
+             updateBlog = {
+                title: this.props.blog.title,
+                author: this.props.blog.author,
+                likes: this.state.likes + 1,
+                url: this.props.blog.url
+            }
+        } else {
+             updateBlog = {
+                user: this.props.blog.user._id,
+                title: this.props.blog.title,
+                author: this.props.blog.author,
+                likes: this.state.likes + 1,
+                url: this.props.blog.url
+
+            }
         }
 
         try {
@@ -36,9 +48,11 @@ class Blog extends React.Component {
 
     }
 
+
+
     render () {
         const showWhenVisible = { display: this.state.visible ? '' : 'none' }
-
+        const showWhenUserDefined = {display: this.props.blog.user === undefined ? 'none' : ''}
         const blogStyle = {
             paddingTop: 10,
             paddingLeft: 2,
@@ -60,7 +74,8 @@ class Blog extends React.Component {
                  <div style={listStyle}>
                      {this.props.blog.url} <br/>
                      {this.state.likes} tykkäystä <button onClick={this.likeBlog}>tykkää</button> <br/>
-                     blogin lisääjä oli {this.props.blog.user.name}
+                   <p style={showWhenUserDefined}> blogin lisääjä oli {this.state.name} </p>
+                     <button>poista</button>
                  </div>
               </div>
             </div>
