55500dafcb82cbe8f88aefc2dae698b9a495a99b
Zilgaro <verner_saaristo@msn.com>
2018-02-26 12:02:05 +0200

    Tehtävä 5.13

--- blogFrontend/src/components/SimpleBlog.test.js
+++ blogFrontend/src/components/SimpleBlog.test.js
@@ -17,4 +17,27 @@ describe.only('<SimpleBlog />', () => {
     expect(titleDiv.text()).toContain(blog.title)
     expect(likesDiv.text()).toContain(blog.likes)
   })
+
+  it('clicking the button twice calls event handler twice', () => {
+    const blog = {
+      title: 'Nappitesti',
+      author: 'Testinappi',
+      likes: '9000'
+    }
+
+    const mockHandler = jest.fn()
+
+    const blogComponent = shallow(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockHandler}
+        />
+    )
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
